66bdd03e90dc76c1b9fe0e4797f87b0a2499d835
Fullajtar <fullajtar.dominik@gmail.com>
2022-08-04 16:03:50 +0200

    Searching for strictly matching tracks, returns TIDAL IDs of tracks

--- PythonScript/create-tidal-playlist.py
+++ PythonScript/create-tidal-playlist.py
@@ -1,4 +1,5 @@
 import tidalapi
+import json
 session = tidalapi.Session()
 
 access_token = None
@@ -35,8 +36,30 @@ else:
     f.write(session.session_id)
     f.close()
 
+# try:
+#     playlist = open('example.json')
+# except:
+#     print("Couldnt load playlist file for transferring to TIDAL")
+#     exit()
 
-result = session.search('album', 'Nectar')
+spotify_playlist_string = '''
+[{"artist_name":"Joji", "track_name":"MODUS", "track_album":"Nectar"},{"artist_name":"Joji", "track_name":"Glimpse of Us", "track_album":"Glimpse of Us"},{"artist_name":"Maduk", "track_name":"Go Back To The Jungle", "track_album":"Transformations"}]
+'''
+spotify_playlist = json.loads(spotify_playlist_string)
+tidal_tracks_IDs = []
 
-for album in result.albums:
-    print(album.artist.name)
+for track_spotify in spotify_playlist:
+    result = session.search('track', track_spotify['track_name'])
+    print('----------------')
+    print('Searching: ',track_spotify['track_name'])
+
+    for track in result.tracks[0:3]:
+        if track.name == track_spotify['track_name'] and track.artist.name == track_spotify['artist_name'] and track.album.name == track_spotify['track_album']:
+            print("BINGO!")
+            # print("TrackName: ",track.name)
+            # print("ArtistName: ",track.artist.name)
+            # print("AlbumName: ",track.album.name)
+            # print(track.id)
+            tidal_tracks_IDs.append(track.id)
+
+print("IDs: ", tidal_tracks_IDs)

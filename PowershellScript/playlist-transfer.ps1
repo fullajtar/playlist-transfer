dd82df61a20aa46f7a2e225a6f2e262bcb247ae4
Fullajtar <fullajtar.dominik@gmail.com>
2022-08-04 13:54:59 +0200

    Feat: Songs info (from playlist) saving to json

--- PowershellScript/playlist-transfer.ps1
+++ PowershellScript/playlist-transfer.ps1
@@ -1,6 +1,6 @@
 $curlpath = 'C:\Windows\System32\curl.exe'
 $playlistId = '0w1Mjpwt3vDVHsFwna20FS'
-$token = "BQBD9RoAG5crfnbpKB5xQLJKMMyGNfzWHhyRaxvMdjS8dMiGLAQZftCZ7i8pdnWzh8tnDSAUbv0fOMZpzw8xwp3ibsUDS7zwgKv-Hb7nW6NvpLmzgNPT2uqpy1WkCda0-B8quSp9gIRSWAsWxv4WZ_FF2xiKa_fMV_D6cAbU8lHDV-Suz1RNDfjf-6xjmSmWXQWeALpUAFMhX5Ux"
+$token = "BQB5feIz1Z_UT8DRdlCEBMLDFVUGlLJ45AYPX3H-5PLD7-guethzc0I7tl0eH3swy0UlvljZlQANat_kpY3ujnELe1p-WqqJ10SzBOaZCJYbs3st_PzVNZgQ-T29GWDWMSSnZag6ipqYVN-8G37DT4RVWd9bmFJLi2ic09mC65hHTR6FAg4UtKO4CELx8Et01RiBV3l5RgQdIpXVOVd3eVu8s_M3F4XgOWM"
 $response = & $curlpath -X "GET" "https://api.spotify.com/v1/playlists/$($playlistId)/tracks" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $($token)"
 $playlist = $response | ConvertFrom-Json
 
@@ -11,5 +11,4 @@ foreach ($song in $playlist.items){
 $jsonString = $jsonString.Substring(0, $jsonString.Length-1)
 $jsonString += ']'
 
-$myJsonObj = $jsonString | ConvertFrom-Json
-Write-Output $myJsonObj
+$jsonString  | Out-File "C:\Users\Fullajtar\Documents\playlist-transfer\example.json"
